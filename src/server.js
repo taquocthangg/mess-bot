9506793d5bffa4c5552c2eba17f5f1ac54726716
taquocthangg <quocthangforwork@gmail.com>
2024-10-26 11:10:13 +0700

    session

--- src/server.js
+++ src/server.js
@@ -5,10 +5,16 @@ import initWebRoutes from "./routes/web";
 import bodyParser from "body-parser";
 import connectDB from "../db";
 import passport from "passport";
+import session from "express-session";
 require('./controllers/passport'); 
 
 let app = express();
-
+app.use(session({
+    secret: '1234abvcd', // Thay thế YOUR_SESSION_SECRET bằng một chuỗi bí mật
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true } // Set secure: true nếu bạn đang sử dụng HTTPS
+}));
 //config body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
